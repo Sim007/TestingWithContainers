f959359c38cd9dd942cfa43dce2c698e4dc807de
Johannes Sim <j.sim18011963@gmail.com>
2022-04-30 12:50:53 +0200

    as fe example

--- EasyExample/cypress/integration/EasyExample_spec.js
+++ EasyExample/cypress/integration/EasyExample_spec.js
@@ -4,7 +4,7 @@ describe('Visit page', () => {
     it('Visit a website', () => {
       cy.visit('/blog/cloud/software-delivery-with-ci-cd-and-containers-continuous-is-the-new-norm/')
     })  
-    it('finds the content "type"', () => {  
+    it('finds text', () => {  
       cy.contains('Johannes Sim')
     })
   })
