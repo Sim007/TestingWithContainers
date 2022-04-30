0fd1278a9540cc89051a23700f7decd596a4b1c1
Johannes Sim <j.sim18011963@gmail.com>
2022-04-30 13:57:24 +0200

    typos

--- EasyExample/cypress/integration/EasyExample_spec.js
+++ EasyExample/cypress/integration/EasyExample_spec.js
@@ -1,8 +1,9 @@
 // Test Easy Example
 
 describe('Visit page', () => {
-    it('Visit a website', () => {
-      cy.visit('/blog/cloud/software-delivery-with-ci-cd-and-containers-continuous-is-the-new-norm')
+    it('Visit craft website page', () => {
+      cy.visit('blog/cloud/software-delivery-with-ci-cd-and-containers-continuous-is-the-new-norm')
+      cy.contains('craft@centric.eu')
     })  
     it('finds text', () => {  
       cy.contains('Johannes Sim')
