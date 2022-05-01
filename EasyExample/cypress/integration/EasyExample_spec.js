a555a398122588cc0793071b923ea334bfb0616a
Johannes Sim <j.sim18011963@gmail.com>
2022-05-01 11:51:43 +0200

    change text in test

--- EasyExample/cypress/integration/EasyExample_spec.js
+++ EasyExample/cypress/integration/EasyExample_spec.js
@@ -3,7 +3,7 @@
 describe('Test craft.centric.eu', () => {
     it('Homepage ', () => {
       cy.visit('/blog/cloud/')
-      cy.contains('craft@centric.eu')
+      cy.contains('Experts in the most important areas within IT')
     })  
     it('Blog of Johannes Sim', () => {  
       cy.visit('/blog/cloud/software-delivery-with-ci-cd-and-containers-continuous-is-the-new-norm/')  
