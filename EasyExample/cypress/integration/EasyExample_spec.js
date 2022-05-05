d13469bac4140e1a8b1f32028aa43fa7da17a084
Johannes Sim <j.sim18011963@gmail.com>
2022-05-05 14:54:09 +0200

    one line test

--- EasyExample/cypress/integration/EasyExample_spec.js
+++ EasyExample/cypress/integration/EasyExample_spec.js
@@ -3,7 +3,6 @@
 describe('Test craft.centric.eu', () => { 
     it('Blog of Johannes Sim', () => {  
       cy.visit('/blog/cloud/software-delivery-with-ci-cd-and-containers-continuous-is-the-new-norm')
-        
-      cy.contains('Johannes Sim')
+//    cy.contains('Johannes Sim')
     })
   })
