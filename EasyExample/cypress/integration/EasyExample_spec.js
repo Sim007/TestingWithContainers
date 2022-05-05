bfc3a79fb15b5975562cefb43f5d823deeed4663
Sim007 <j.sim18011963@gmail.com>
2022-05-05 15:53:59 +0200

    back

--- EasyExample/cypress/integration/EasyExample_spec.js
+++ EasyExample/cypress/integration/EasyExample_spec.js
@@ -1,8 +1,8 @@
 // Test Easy Example
 
 describe('Test craft.centric.eu', () => { 
-    it('Visit Blog of Johannes Sim', () => {  
-      cy.visit('http://craft.centric.eu/blog/cloud/software-delivery-with-ci-cd-and-containers-continuous-is-the-new-norm')
-      cy.contains('Johannes Sim')
+  it('Visit Blog of Johannes Sim', () => {  
+    cy.visit('http://craft.centric.eu/blog/cloud/software-delivery-with-ci-cd-and-containers-continuous-is-the-new-norm')
+    cy.contains('Johannes Sim')
     }) 
   })
