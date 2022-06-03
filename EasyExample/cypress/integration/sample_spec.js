08e30828df4562dcdad3289b699cb1f674a2fe48
Johannes Sim <j.sim18011963@gmail.com>
2022-06-03 18:08:05 +0200

    linkedin page

--- EasyExample/cypress/integration/sample_spec.js
+++ EasyExample/cypress/integration/sample_spec.js
@@ -3,7 +3,7 @@ describe('My First Test', () => {
       expect(true).to.equal(true)
     }),
     it('visit a page', () => {
-      cy.visit('https://craft.centric.eu/blog/cloud/software-delivery-with-ci-cd-and-containers-continuous-is-the-new-norm/')
+      cy.visit('https://nl.linkedin.com/in/johannes-sim-cd')
       cy.contains('Johannes Sim')
     })
   })
